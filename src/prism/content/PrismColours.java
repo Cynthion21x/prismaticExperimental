84c879a8e9332846763cb26ac4c3ffb530bd10e1
ice100k <ice100k@outlook.com>
2022-01-05 13:43:38 +0000

    more colours

--- src/prism/content/PrismColours.java
+++ src/prism/content/PrismColours.java
@@ -5,9 +5,11 @@ import arc.graphics.Colors;
 import mindustry.graphics.Pal;
 
 public class NHColor{
+
 	public static Color
 
 		hotPink = Color.valueOf("#ff33cc"),
+		impureGreen = Color.valueOf("#385947")
 
 	static{
 		Colors.put("heal", Pal.heal);

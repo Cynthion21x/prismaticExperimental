6959476474583a600d2b66221a0eaee647ada416
thecrisperson1 <62443519+thecrisperson1@users.noreply.github.com>
2022-01-05 17:52:27 +0000

    sneaky ( hiding from me

--- src/prism/content/PrismBlocks.java
+++ src/prism/content/PrismBlocks.java
@@ -51,7 +51,7 @@ public class PrismBlocks implements ContentList{
 
       consumes.power(1.8f);
       consumes.items(with(Items.titanium, 2));
-      consumes.liquids(PrismLiquids.dwave, 0.2f));
+      consumes.liquids(PrismLiquids.dwave, 0.2f);
 
       itemCapacity = 30;
 			liquidCapacity = 30;

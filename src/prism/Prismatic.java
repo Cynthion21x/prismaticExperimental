88d61a04bd53b85b00fbcfd749c2e46e5c5e7e94
ice100k <ice100k@outlook.com>
2022-01-05 13:17:53 +0000

    sprites

---
+++ src/prism/Prismatic.java
@@ -0,0 +1,47 @@
+package prism
+
+import arc.*;
+import arc.util.*;
+import mindustry.*;
+import mindustry.content.*;
+import mindustry.game.EventType.*;
+import mindustry.gen.*;
+import mindustry.mod.*;
+import mindustry.ui.dialogs.*;
+
+public class Prismatic extends Mod{
+
+  public Prismatic(){
+
+      Log.info("Loaded Prismatic constructor.");
+
+
+      Events.on(ClientLoadEvent.class, e -> {
+
+          //change Log
+
+          Time.runTask(10f, () -> {
+              BaseDialog dialog = new BaseDialog("Change Log");
+
+              dialog.cont.add("- Port from Hjosn to Java").row();
+
+              dialog.cont.button("Confirm", dialog::hide).size(100f, 50f);
+              dialog.show();
+          });
+      });
+  }
+
+  private static final ContentList[] content = {
+    new PrismStatusEffects()
+  };
+
+  @Override
+  public void loadContent(){
+      Log.info("Loading content..");
+
+
+
+      Log.info("Content Loaded!");
+  }
+
+}

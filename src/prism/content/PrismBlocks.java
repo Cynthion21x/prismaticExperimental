0db865ac03eef57a0748ebefb6450652a54003c4
thecrisperson1 <62443519+thecrisperson1@users.noreply.github.com>
2022-01-05 17:19:06 +0000

    i have no idea why the compressor wont take in diron

--- src/prism/content/PrismBlocks.java
+++ src/prism/content/PrismBlocks.java
@@ -39,18 +39,18 @@ public class PrismBlocks implements ContentList{
   public void load(){
 
     dcompressor = new GenericCrafter("dcompressor"){{
-    requirements(Category.crafting, with(Items.lead, 120, Items.copper, 60, Items.titanium, 100, Items.silicon, 40));
+      requirements(Category.crafting, with(Items.lead, 120, Items.copper, 60, Items.titanium, 100, Items.silicon, 40));
 
-    size = 2;
-    hasPower = true;
-    hasLiquids = false;
-    hasItems = true;
-    craftTime = 90;
-    craftEffect = Fx.smeltsmoke;
-    outputItem = new ItemStack(PrismItems.diron, 1);
+      size = 2;
+      hasPower = true;
+      hasLiquids = false;
+      hasItems = true;
+      craftTime = 90;
+      craftEffect = Fx.smeltsmoke;
+      outputItem = new ItemStack(PrismItems.diron, 1);
 
-    consumes.power(1.8f);
-    consumes.items(with(Items.titanium, 2));
+      consumes.power(1.8f);
+      consumes.items(with(Items.titanium, 2));
     }};
 
 

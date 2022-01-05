386fefb39bd6d8238d1c67621f2890053edbd287
thecrisperson1 <62443519+thecrisperson1@users.noreply.github.com>
2022-01-05 16:27:13 +0000

    you cant have - : /

--- src/prism/content/PrismItems.java
+++ src/prism/content/PrismItems.java
@@ -5,11 +5,11 @@ import mindustry.ctype.*;
 import mindustry.type.*;
 
 public class PrismItems implements ContentList{
-	public static Item d-iron;
+	public static Item diron;
 
 	@Override
     public void load(){
-    	d-iron = new Item("d-iron", Color.valueOf("c42b9e")){{
+    	diron = new Item("d-iron", Color.valueOf("c42b9e")){{
             cost = 2f;
         }};
 

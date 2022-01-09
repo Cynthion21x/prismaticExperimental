cbee74eb259df778facaaaa4aa75a604ed29899f
ice100k <ice100k@outlook.com>
2022-01-09 14:11:19 +0000

    test

---
+++ src/prism/content/PrismFX.java
@@ -0,0 +1,48 @@
+package Prism.content;
+
+import arc.*;
+import arc.graphics.*;
+import arc.graphics.g2d.*;
+import arc.math.*;
+import arc.math.geom.*;
+import arc.struct.*;
+import arc.util.*;
+import mindustry.entities.*;
+import mindustry.game.*;
+import mindustry.gen.*;
+import mindustry.graphics.*;
+import mindustry.type.*;
+import mindustry.world.Block;
+
+import static arc.graphics.g2d.Draw.rect;
+import static arc.graphics.g2d.Draw.*;
+import static arc.graphics.g2d.Lines.*;
+import static arc.math.Angles.*;
+import static mindustry.Vars.*;
+
+public class PrismFX{
+
+  public static final Effect
+
+  none = new Effect(0, 0f, e -> {}),
+
+  missileStrike = new Effect(40f, 100f, e -> {
+      color(Color.valueOf("#FF33CC"));
+      stroke(e.fout() * 2f);
+      float circleRad = 4f + e.finpow() * 65f;
+      Lines.circle(e.x, e.y, circleRad);
+
+      color(Color.valueOf("#FF33CC"));
+      for(int i = 0; i < 4; i++){
+          Drawf.tri(e.x, e.y, 6f, 100f * e.fout(), i*90);
+      }
+
+      color();
+      for(int i = 0; i < 4; i++){
+          Drawf.tri(e.x, e.y, 3f, 35f * e.fout(), i*90);
+      }
+
+      Drawf.light(e.x, e.y, circleRad * 1.6f, Pal.heal, e.fout());
+  });
+
+}
---
+++ src/prism/entities/bullets/StrikeMissileBulletType.java
@@ -0,0 +1,43 @@
+package prism.entities.bullets;
+
+import arc.graphics.*;
+import arc.graphics.g2d.*;
+import arc.math.*;
+import arc.util.*;
+import prism.content.*;
+import mindustry.content.*;
+import mindustry.entities.bullet.*;
+import mindustry.gen.*;
+import mindustry.graphics.*;
+import mindustry.type.*;
+
+import static arc.Core.atlas;
+import prism.content.*;
+
+public class StrikeMissileBulletType extends BulletType {
+
+    public StrikeMissileBulletType(float speed, float damage, StatusEffect status){
+
+      this.speed = speed;
+      this.damage = damage;
+      this.status = status;
+
+      pierce = pierceBuilding = false;
+
+      absorbable = true;
+      hittable = true;
+      collidesTiles = false;
+      lifetime = 120f;
+      hitSize = 15f;
+      lightRadius = 5f;
+
+      lightColor = hitColor;
+
+      buildingDamageMultiplier = 0f;
+
+      shootEffect = smokeEffect = PrismFX.none;
+      despawnEffect = PrismFX.missileStrike;
+
+    }
+
+}

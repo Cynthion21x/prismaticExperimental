ba35d16d7948e012cc0bd7d4dab14d7e5ef31fc5
ice100k <ice100k@outlook.com>
2022-01-15 11:28:19 +0000

    effect not working

--- src/prism/content/PrismFX.java
+++ src/prism/content/PrismFX.java
@@ -44,7 +44,7 @@ public class PrismFX{
       Drawf.light(e.x, e.y, circleRad * 1.6f, Pal.heal, e.fout());
   }),
 
-  SeigeCharge = new Effect(80f, 100f, e -> {
+  SeigeCharge = new Effect(800f, 100f, e -> {
         color(Color.valueOf("#FF33CC"));
         stroke(e.fin() * 2f);
         Lines.circle(e.x, e.y, 4f + e.fout() * 100f);
@@ -60,7 +60,7 @@ public class PrismFX{
 
         Fill.circle(e.x, e.y, e.fin() * 10);
         Drawf.light(e.x, e.y, e.fin() * 20f, Pal.heal, 0.7f);
-    }).followParent(true).rotWithParent(true)
+    })
 
   ;
 

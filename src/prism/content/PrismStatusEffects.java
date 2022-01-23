dfd954be8a264d87c7f9a90120cd09ccef5c9098
ice100k <ice100k@outlook.com>
2022-01-23 10:58:17 +0000

    test

--- src/prism/content/PrismStatusEffects.java
+++ src/prism/content/PrismStatusEffects.java
@@ -29,33 +29,6 @@ public class PrismStatusEffects implements ContentList {
   @Override
   public void load() {
 
-    charge = new StatusEffect("charge") {{
-
-        //disarm = true;
-        speedMultiplier = 0.0f;
-        healthMultiplier = 2f;
-        buildSpeedMultiplier = 0f;
-
-        transitionDamage = 90f;
-
-        color = Color.valueOf("#BA03fC");
-    }};
-
-    shatter = new StatusEffect("shatter") {{
-
-        //disarm = true;
-        show = true;
-
-        reloadMultiplier = 0.2f;
-        buildSpeedMultiplier = 1f;
-        damageMultiplier = 0.8f;
-
-        transitionDamage = 146f;
-
-        color = Color.valueOf("#A1FFFF");
-    }};
-
-
     crippled = new StatusEffect("crippled") {{
         damage = 5f;
         speedMultiplier = 0.5f;
@@ -65,27 +38,18 @@ public class PrismStatusEffects implements ContentList {
         // Color = PrismColours.hotPink;
         color = Color.valueOf("#FF33CC");
 
-        init(() -> {
+      /*  init(() -> {
 
           affinity(StatusEffects.blasted, (unit, result, time) -> {
               unit.damagePierce(transitionDamage);
               result.set(shatter, time + result.time);
           });
 
-      });
+      }); */
 
     }};
 
-    sink = new StatusEffect("sink") {{
-        show = true;
 
-        reloadMultiplier = 0.5f;
-        speedMultiplier = 0.3f;
-        buildSpeedMultiplier = 0f;
-        healthMultiplier = 0.5f;
-
-        color = Color.valueOf("#120054");
-    }};
 
   }
 }
